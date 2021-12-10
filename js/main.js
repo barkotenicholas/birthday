3a44b875bd775ef48358f1c883adfa923620a547
barkotenicholas <barkotenicholas@gmail.com>
2021-12-10 20:33:30 +0300

    linked external constant js to main.js

--- js/main.js
+++ js/main.js
@@ -1,78 +1,120 @@
-const date = document.getElementById('birthday')
-const male = document.getElementById('male')
-const female = document.getElementById('female')
-const form = document.getElementById('forms')
+const date = document.getElementById('birthday');
+const male = document.getElementById('male');
+const female = document.getElementById('female');
+const form = document.getElementById('forms');
 
+import {
+    femaleNames,
+    maleNames
+} from './const.js'
 
+var defaultdate;
+var currentdate;
+var gender;
 // validate function
-form.addEventListener('submit',(e)=>{
+form.addEventListener('submit', (e) => {
 
-    e.preventDefault()
-    if(validate()){
-        
+    e.preventDefault();
+
+    if (validate()) {
+
+        var dateObject = new Date(currentdate);
+        var day = dateObject.getDay();
+        console.log(day);
+
+        if (male.checked) {
+            gender = "male";
+            console.log(gender)
+        } else if (female.checked) {
+            gender = "female";
+            console.log(gender)
+
+        }
+        if (gender == "male") {
+
+            var nameObj = maleNames[day];
+            var name = nameObj.name;
+            var para = document.getElementById('ans');
+
+            para.innerHTML = "Your Male Akan name is " + name;
+        } else if (gender == "female") {
+
+            var nameObj = femaleNames[day];
+            var name = nameObj.name;
+            var para = document.getElementById('ans');
+            para.innerHTML = "Your Female Akan name is " + name;
+
+        }
+
     }
 })
-function validate(){
 
-    let defaultdate = date.defaultValue;
-    let currentdate = date.value;
+function validate() {
+
+    defaultdate = date.defaultValue;
+    currentdate = date.value;
 
 
+    if ((defaultdate === currentdate) && (!male.checked && !female.checked)) {
 
+        setError("Input valid email.", date);
+        setGenderError("Please check one gender", male);
+        return false;
 
-    if((defaultdate === currentdate) && (!male.checked && !female.checked)){
-        setError("Input valid email." ,date)
-        setGenderError("Please check one gender",male)
+    } else if (defaultdate === currentdate) {
 
-    }
-    else if(defaultdate === currentdate){
-        setError("Input valid email." ,date)
+        setError("Input valid email.", date);
+        return false;
 
-    }else if(!male.checked && !female.checked){
-        setGenderError("Please check one gender",male)
-    }
-    else{
-        success("Valid email.",date)
-        setGenderSuccess("Success ", male)
+    } else if (!male.checked && !female.checked) {
 
+        setGenderError("Please check one gender", male);
+        return false;
+
+    } else {
+
+        success("Valid email.", date);
+        setGenderSuccess("Success ", male);
         return true;
+
     }
 
 
 
-  
-    
+
+
 }
 
-function setError(message,input){
+function setError(message, input) {
 
-    const formControl =input.parentElement;
+    const formControl = input.parentElement;
     const small = formControl.querySelector('small.birthdaymsg');
     const ierror = formControl.querySelector('i.failureemail');
     small.innerHTML = message
     ierror.classList.remove("invisible")
 }
 
-function success(message,input){
-    const formControl =input.parentElement;
+function success(message, input) {
+    const formControl = input.parentElement;
     const small = formControl.querySelector('small.errormsg');
     const ierror = formControl.querySelector('i.successemail');
     small.innerHTML = message
     ierror.classList.remove("invisible")
 }
 
-function setGenderError(message,input){
+function setGenderError(message, input) {
 
-    const formControl =input.parentElement;
+    const formControl = input.parentElement;
     const small = formControl.querySelector('small.errormsg');
     const ierror = formControl.querySelector('i.failuregender');
     small.innerHTML = message
     ierror.classList.remove("invisible")
 
 }
-function setGenderSuccess(message,input){
 
-    const formControl =input.parentElement;
+function setGenderSuccess(message, input) {
+
+    const formControl = input.parentElement;
     const small = formControl.querySelector('small.errormsg');
     const ierror = formControl.querySelector('i.successgender');
     small.innerHTML = message
