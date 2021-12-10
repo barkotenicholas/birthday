df8f7fbe63d937bfc6fcf51cd7999e142b83834f
barkotenicholas <barkotenicholas@gmail.com>
2021-12-10 22:50:36 +0300

    Added appropriate spacing

--- js/const.js
+++ js/const.js
@@ -1,5 +1,50 @@
-const femaleNames = [{"day":"Sunday","name":"Akosua"},{"day":"Monday","name":"Adwoa"},{"day":"Tuesday","name":"Abenaa"},{"day":"Wednesday","name":"Akua"},{"day":"Thursday","name":"Yaa"},{"day":"Friday","name":"Afua"},{"day":"Saturday","name":"Ama"}];
+const femaleNames = [{
+    "day": "Sunday",
+    "name": "Akosua"
+}, {
+    "day": "Monday",
+    "name": "Adwoa"
+}, {
+    "day": "Tuesday",
+    "name": "Abenaa"
+}, {
+    "day": "Wednesday",
+    "name": "Akua"
+}, {
+    "day": "Thursday",
+    "name": "Yaa"
+}, {
+    "day": "Friday",
+    "name": "Afua"
+}, {
+    "day": "Saturday",
+    "name": "Ama"
+}];
 
-const maleNames = [{"day":"Sunday","name":"Kwasi"},{"day":"Monday","name":"Kwadwo"},{"day":"Tuesday","name":"Kwabena"},{"day":"Wednesday","name":"Kwaku"},{"day":"Thursday","name":"Yaw"},{"day":"Friday","name":"Kofi"},{"day":"Saturday","name":"Kwame"}];
+const maleNames = [{
+    "day": "Sunday",
+    "name": "Kwasi"
+}, {
+    "day": "Monday",
+    "name": "Kwadwo"
+}, {
+    "day": "Tuesday",
+    "name": "Kwabena"
+}, {
+    "day": "Wednesday",
+    "name": "Kwaku"
+}, {
+    "day": "Thursday",
+    "name": "Yaw"
+}, {
+    "day": "Friday",
+    "name": "Kofi"
+}, {
+    "day": "Saturday",
+    "name": "Kwame"
+}];
 
-export {femaleNames,maleNames};
+export {
+    femaleNames,
+    maleNames
+};
