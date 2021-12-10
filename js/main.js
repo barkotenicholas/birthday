ef37c724205e2592689885f5329df16e0b2f0827
barkotenicholas <barkotenicholas@gmail.com>
2021-12-10 15:34:14 +0300

    Added js validation

--- js/main.js
+++ js/main.js
@@ -8,12 +8,53 @@ function validate(){
     let defaultdate = date.defaultdate;
     let currentdate = date.value;
 
-    if(defaultdate == currentdate){
-        setError("Please input date " ,date)
+    if(!male.checked && !female.checked){
+        setGenderError("Please check one gender",male)
+    }else{
+        setGenderSuccess("Success ", male)
     }
+
+    if(defaultdate == currentdate){
+        setError("Input valid email." ,date)
+    }
+    else{
+        success("Valid email.",date)
+    }
+
     
 }
 
 function setError(message,input){
-    
-}   
+
+    const formControl =input.parentElement;
+    const small = formControl.querySelector('small.birthdaymsg');
+    const ierror = formControl.querySelector('i.failureemail');
+    small.innerHTML = message
+    ierror.classList.remove("invisible")
+}
+
+function success(message,input){
+    const formControl =input.parentElement;
+    const small = formControl.querySelector('small.errormsg');
+    const ierror = formControl.querySelector('i.successemail');
+    small.innerHTML = message
+    ierror.classList.remove("invisible")
+}
+
+function setGenderError(message,input){
+
+    const formControl =input.parentElement;
+    const small = formControl.querySelector('small.errormsg');
+    const ierror = formControl.querySelector('i.failuregender');
+    small.innerHTML = message
+    ierror.classList.remove("invisible")
+
+}
+function setGenderSuccess(message,input){
+    const formControl =input.parentElement;
+    const small = formControl.querySelector('small.errormsg');
+    const ierror = formControl.querySelector('i.successgender');
+    small.innerHTML = message
+    ierror.classList.remove("invisible")
+
+}
